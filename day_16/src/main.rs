b54d948846d2c1ad779c12789e1ad52df24824ed
SamuelForbes <samuelforbes@live.com>
2025-01-30 20:25:40 +0000

    Day 16 complete. In need of re-work

--- day_16/src/main.rs
+++ day_16/src/main.rs
@@ -1,5 +1,6 @@
 use crate::Direction::{East, North, South, West};
-use std::collections::HashSet;
+use itertools::Itertools;
+use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
 
@@ -29,12 +30,12 @@ impl Direction {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Path {
     current_direction: Direction,
     current_location: (usize, usize),
-    visited_locations: HashSet<(usize, usize)>,
     running_score: usize,
+    path_tiles: Vec<(usize, usize)>,
 }
 
 impl Path {
@@ -42,96 +43,157 @@ impl Path {
         Path {
             current_direction: direction,
             current_location,
-            visited_locations: HashSet::new(),
             running_score: 0,
+            path_tiles: Vec::new(),
         }
     }
 }
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    
+
     let start = Instant::now();
     println!("Part One: {} - {:?}", part_one(&input), start.elapsed());
+
+    let start = Instant::now();
+    println!("Part Two: {} - {:?}", part_two(&input), start.elapsed());
 }
 
 fn part_one(input: &str) -> usize {
     let map = parse_input(input);
 
-    find_paths(&map)
+    *find_paths(&map)
+        .iter()
+        .map(|(value, _)| value)
+        .min()
+        .unwrap()
 }
 
-fn find_paths(map: &Vec<Vec<char>>) -> usize {
+fn part_two(input: &str) -> usize {
+    let mut map = parse_input(input);
+    let valid_paths = find_paths(&map);
+    let min = valid_paths.iter().map(|(value, _)| value).min().unwrap();
+
+    let unique_tiles = valid_paths
+        .iter()
+        .filter(|(value, _)| value == min)
+        .flat_map(|(_, set)| set.iter().collect::<Vec<&(usize, usize)>>())
+        .unique()
+        .collect::<Vec<&(usize, usize)>>();
+
+    for unique_tile in &unique_tiles {
+        map[unique_tile.1][unique_tile.0] = 'O';
+    }
+
+    unique_tiles.len()
+}
+
+fn find_paths(map: &Vec<Vec<char>>) -> Vec<(usize, Vec<(usize, usize)>)> {
     let mut start_path = Path::new(East, find_start(map));
-    let mut solutions = vec!();
+    let mut solutions = vec![];
+    let mut visited_locations = HashMap::new();
 
-    find_shortest_path_to_finish(map, &mut start_path, &mut solutions);
-    
-    *solutions.iter().min().unwrap()
+    find_shortest_path_to_finish(map, &mut visited_locations, &mut start_path, &mut solutions);
+
+    solutions
 }
 
-fn find_shortest_path_to_finish(map: &Vec<Vec<char>>, path: &mut Path, solutions: &mut Vec<usize>) {
-    if path.visited_locations.contains(&path.current_location) {
+fn find_shortest_path_to_finish(
+    map: &Vec<Vec<char>>,
+    visited_locations: &mut HashMap<(usize, usize), usize>,
+    path: &mut Path,
+    solutions: &mut Vec<(usize, Vec<(usize, usize)>)>,
+) {
+    if square_already_visited(path) || path_longer_than_existing_solution(path, solutions) {
         return;
     }
-    
+
     if reached_end(map, path) {
-        solutions.push(path.running_score);
+        path.path_tiles.push(path.current_location);
+        solutions.push((path.running_score, path.path_tiles.clone()));
         return;
     } else {
-        path.visited_locations.insert(path.current_location);
+        visited_locations.insert(path.current_location, path.running_score);
+        path.path_tiles.push(path.current_location);
     }
-    
-    if available_turns(map, path) {
-        path.current_direction
-            .get_turn_directions()
-            .iter()
-            .filter(|direction| {
-                let location_to_check = direction.get_location_in_direction(path.current_location);
-                map[location_to_check.1][location_to_check.0] == '.'
-            })
-            .for_each(|direction| {
-                let mut new_path = Path {
-                    current_direction: direction.clone(),
-                    running_score: path.running_score + 1001,
-                    visited_locations: path.visited_locations.clone(),
-                    current_location: direction.get_location_in_direction(path.current_location)
-                };
-                
-                find_shortest_path_to_finish(map, &mut new_path, solutions);
-            });
+
+    make_turns_if_available(map, visited_locations, path, solutions);
+
+    if square_visited_cheaper(path, visited_locations) {
+        return;
     }
 
     if clear_space_ahead(map, path) {
-        path.current_location = path.current_direction.get_location_in_direction(path.current_location);
+        path.current_location = path
+            .current_direction
+            .get_location_in_direction(path.current_location);
+
         path.running_score += 1;
-        find_shortest_path_to_finish(map, path, solutions);
+        find_shortest_path_to_finish(map, visited_locations, path, solutions);
     }
 }
 
-fn clear_space_ahead(map: &Vec<Vec<char>>, path: &Path) -> bool {
-    let new_position = path.current_direction.get_location_in_direction(path.current_location);
+fn path_longer_than_existing_solution(path: &mut Path, solutions: &mut [(usize, Vec<(usize, usize)>)]) -> bool {
+    !solutions.is_empty() && solutions.iter().min_by(|a, b| a.0.cmp(&b.0)).unwrap().0 < path.running_score
+}
+
+fn square_already_visited(path: &Path) -> bool {
+    path.path_tiles.contains(&path.current_location)
+}
+
+fn square_visited_cheaper(
+    path: &Path,
+    visited_locations: &mut HashMap<(usize, usize), usize>,
+) -> bool {
+    visited_locations.contains_key(&path.current_location)
+        && visited_locations.get(&path.current_location).unwrap() < &path.running_score
+}
+
+fn make_turns_if_available(
+    map: &Vec<Vec<char>>,
+    visited_locations: &mut HashMap<(usize, usize), usize>,
+    path: &mut Path,
+    solutions: &mut Vec<(usize, Vec<(usize, usize)>)>,
+) {    
+    for direction in path.current_direction.get_turn_directions().iter() {
+        if location_worth_visiting(direction, map, &visited_locations, &path) {
+            let mut new_path = Path {
+                current_direction: direction.clone(),
+                running_score: path.running_score + 1001,
+                current_location: direction.get_location_in_direction(path.current_location),
+                path_tiles: path.path_tiles.clone(),
+            };
+
+            find_shortest_path_to_finish(map, visited_locations, &mut new_path, solutions);
+        }
+    }
+}
+
+fn location_worth_visiting(
+    direction: &Direction,
+    map: &[Vec<char>],
+    visited_locations: &&mut HashMap<(usize, usize), usize>,
+    path: &&mut Path,
+) -> bool {
+    let location_to_check = direction.get_location_in_direction(path.current_location);
+
+    map[location_to_check.1][location_to_check.0] == '.'
+        && (!visited_locations.contains_key(&location_to_check)
+            || visited_locations.get(&location_to_check).unwrap() >= &(path.running_score + 1001))
+}
+
+fn clear_space_ahead(map: &[Vec<char>], path: &Path) -> bool {
+    let new_position = path
+        .current_direction
+        .get_location_in_direction(path.current_location);
     map[new_position.1][new_position.0] != '#'
 }
 
-fn reached_end(map: &Vec<Vec<char>>, path: &Path) -> bool {
+fn reached_end(map: &[Vec<char>], path: &Path) -> bool {
     map[path.current_location.1][path.current_location.0] == 'E'
 }
 
-fn available_turns(map: &Vec<Vec<char>>, path: &Path) -> bool {
-    let available_turns = path.current_direction
-        .get_turn_directions()
-        .iter()
-        .filter(|direction| {
-            let location_to_check = direction.get_location_in_direction(path.current_location);
-            map[location_to_check.1][location_to_check.0] == '.'
-        })
-        .count();
-    
-    available_turns > 0
-}
-
-fn find_start(map: &Vec<Vec<char>>) -> (usize, usize) {
+fn find_start(map: &[Vec<char>]) -> (usize, usize) {
     let mut start = (0_usize, 0_usize);
 
     map.iter().enumerate().for_each(|(i, row)| {
@@ -166,6 +228,7 @@ fn small_input() {
 ###############";
 
     assert_eq!(7036, part_one(input));
+    assert_eq!(45, part_two(input));
 }
 
 #[test]
@@ -189,4 +252,5 @@ fn bigger_input() {
 #################";
 
     assert_eq!(11048, part_one(input));
+    assert_eq!(64, part_two(input));
 }
